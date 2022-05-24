3927c8c96def1b4f72f1604836d77c7cd4f9e8f3
md-ashikul-islam <md.ashikul.islam@g.bracu.ac.bd>
2022-05-24 14:46:31 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const handleResize = () => setWidth(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="App">
-      <h1>App</h1>
-      <p>Window Width: {window.innerWidth}px</p>
+      <h1>{width < 500 ? "Mobile App" : "App"}</h1>
+      <p>Window Width: {width}px</p>
     </div>
   );
 }
